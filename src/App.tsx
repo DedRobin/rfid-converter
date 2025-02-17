c9ca3bd16968703c07387e6f26930438c8dcd3e2
Pavel Rabekin <pavelrabekin@gmail.com>
2025-02-17 15:16:33 +0300

    feat: deploy app №1

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import Converter from './components/Converter';
-import './App.css';
-
-function App() {
-  return <Converter />;
-}
-
-export default App;
--- src/tools/character.ts
+++
@@ -1,5 +0,0 @@
-const isNumber = (char: string) => '0123456789'.includes(char);
-const isAlpha = (char: string) =>
-  'abcdefghijklmnopqrstuvwxyz'.includes(char.toLocaleLowerCase());
-
-export { isNumber, isAlpha };
