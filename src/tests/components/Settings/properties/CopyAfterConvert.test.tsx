f59f70cacce868144a17033786c6336be374c4fd
Pavel Rabekin <pavelrabekin@gmail.com>
2025-11-13 16:11:45 +0300

    Fix: Settings - label displaying (#10)
    * fix: fix setting collapsed button nad tests
    
    * fix: fix setting collapsed button and tests 2
    
    * test: add additional test (copyAfterConvert checkbox)
    
    * test: add test <CopyAfterConvert/> - label appers after clicking on 'Settings' button

--- src/tests/components/Settings/properties/CopyAfterConvert.test.tsx
+++ src/tests/components/Settings/properties/CopyAfterConvert.test.tsx
@@ -1,4 +1,5 @@
 import CopyAfterConvert from '@components/Main/Converter/Settings/properties/CopyAfterConvert';
+import { resources } from '@shared/i18n';
 import store from '@store/index';
 import { fireEvent } from '@testing-library/dom';
 import { renderWithProviders } from '@tests/utils';
@@ -12,12 +13,39 @@ describe('CopyAfterConvert.test Component', () => {
     expect(copyAfterConvert).toBeInTheDocument();
   });
 
+  it('state should has init value when checkbox is checked', () => {
+    const { getByTestId, getByRole } = renderWithProviders(
+      <CopyAfterConvert />
+    );
+
+    const checkbox = getByRole('checkbox', {
+      name: resources.en.translation.settings.copyAfterConvert,
+    });
+    expect(checkbox).toBeInTheDocument();
+    fireEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    expect(store.getState().settings.copyAfterConvert).toEqual('text');
+
+    const copyAfterConvert = getByTestId('copy-after-convert-test-id');
+    expect(copyAfterConvert).toHaveTextContent(
+      resources.en.translation.settings.copyAfterConvert
+    );
+  });
+
   it('should select HEX option', () => {
     const { getByTestId, getByRole } = renderWithProviders(
       <CopyAfterConvert />
     );
-    const copyAfterConvert = getByTestId('copy-after-convert-test-id');
 
+    // const checkbox = getByRole('checkbox', {
+    //   name: resources.en.translation.settings.copyAfterConvert,
+    // });
+    // expect(checkbox).toBeInTheDocument();
+    // fireEvent.click(checkbox);
+
+    const copyAfterConvert = getByTestId(
+      'copy-after-convert-radio-buttons-test-id'
+    );
     expect(copyAfterConvert).toBeInTheDocument();
 
     const hexButton = getByRole('radio', { name: 'HEX' });
@@ -29,12 +57,7 @@ describe('CopyAfterConvert.test Component', () => {
   });
 
   it('should select DEX option', () => {
-    const { getByTestId, getByRole } = renderWithProviders(
-      <CopyAfterConvert />
-    );
-    const copyAfterConvert = getByTestId('copy-after-convert-test-id');
-
-    expect(copyAfterConvert).toBeInTheDocument();
+    const { getByRole } = renderWithProviders(<CopyAfterConvert />);
 
     const hexButton = getByRole('radio', { name: 'DEX' });
     expect(hexButton).toBeInTheDocument();
@@ -45,18 +68,11 @@ describe('CopyAfterConvert.test Component', () => {
   });
 
   it('should select TEXT option', () => {
-    const { getByTestId, getByRole } = renderWithProviders(
-      <CopyAfterConvert />
-    );
-    const copyAfterConvert = getByTestId('copy-after-convert-test-id');
-
-    expect(copyAfterConvert).toBeInTheDocument();
+    const { getByRole } = renderWithProviders(<CopyAfterConvert />);
 
     const hexButton = getByRole('radio', { name: 'TEXT' });
     expect(hexButton).toBeInTheDocument();
-
     fireEvent.click(hexButton);
-
     expect(store.getState().settings.copyAfterConvert).toEqual('text');
   });
 });
