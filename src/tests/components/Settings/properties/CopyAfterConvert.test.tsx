ffc95fc3c13abe4b466420b661e71df47a0effec
Pavel Rabekin <pavelrabekin@gmail.com>
2025-11-13 13:57:36 +0300

    test: update tests for <CopyAfterConvert/>

--- src/tests/components/Settings/properties/CopyAfterConvert.test.tsx
+++ src/tests/components/Settings/properties/CopyAfterConvert.test.tsx
@@ -1,4 +1,6 @@
 import CopyAfterConvert from '@components/Main/Converter/Settings/properties/CopyAfterConvert';
+import store from '@store/index';
+import { fireEvent } from '@testing-library/dom';
 import { renderWithProviders } from '@tests/utils';
 import { describe, expect, it } from 'vitest';
 
@@ -8,5 +10,53 @@ describe('CopyAfterConvert.test Component', () => {
     const copyAfterConvert = getByTestId('copy-after-convert-test-id');
 
     expect(copyAfterConvert).toBeInTheDocument();
+  });
+
+  it('should select HEX option', () => {
+    const { getByTestId, getByRole } = renderWithProviders(
+      <CopyAfterConvert />
+    );
+    const copyAfterConvert = getByTestId('copy-after-convert-test-id');
+
+    expect(copyAfterConvert).toBeInTheDocument();
+
+    const hexButton = getByRole('radio', { name: 'HEX' });
+    expect(hexButton).toBeInTheDocument();
+
+    fireEvent.click(hexButton);
+
+    expect(store.getState().settings.copyAfterConvert).toEqual('hex');
+  });
+
+  it('should select DEX option', () => {
+    const { getByTestId, getByRole } = renderWithProviders(
+      <CopyAfterConvert />
+    );
+    const copyAfterConvert = getByTestId('copy-after-convert-test-id');
+
+    expect(copyAfterConvert).toBeInTheDocument();
+
+    const hexButton = getByRole('radio', { name: 'DEX' });
+    expect(hexButton).toBeInTheDocument();
+
+    fireEvent.click(hexButton);
+
+    expect(store.getState().settings.copyAfterConvert).toEqual('dex');
+  });
+
+  it('should select TEXT option', () => {
+    const { getByTestId, getByRole } = renderWithProviders(
+      <CopyAfterConvert />
+    );
+    const copyAfterConvert = getByTestId('copy-after-convert-test-id');
+
+    expect(copyAfterConvert).toBeInTheDocument();
+
+    const hexButton = getByRole('radio', { name: 'TEXT' });
+    expect(hexButton).toBeInTheDocument();
+
+    fireEvent.click(hexButton);
+
+    expect(store.getState().settings.copyAfterConvert).toEqual('text');
   });
 });
